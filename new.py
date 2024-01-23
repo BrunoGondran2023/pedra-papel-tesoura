dd82f6ac3b36b11652319aaf5df45df50be178ee
Bruno Gondran <brunogondran@gmail.com>
2024-01-23 19:08:44 -0300

    Criação da função para calcular tabuada.

--- new.py
+++ new.py
@@ -1,1 +1,6 @@
-print('Ola, Mundo!') 
+def tabuada():
+    for num1 in range(1, 11):
+        print(f'\nTabuada do número: {num1}\n')
+        for num2 in range(1, 11):
+            print(f'{num1} X {num2} = {num1 * num2}')
+tabuada()
